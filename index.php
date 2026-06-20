3e78eb96789382688c75084e9316a81d273fc149
Rowen <rowen7908@gmail.com>
2026-06-20 21:49:57 +0800

    add index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Greetings Pages</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <h1>Hello</h1>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
